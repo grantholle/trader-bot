47cf5063e34e4db0661463a5afb9324bf690c90b
Grant Holle <hollegrant@gmail.com>
2018-04-04 09:54:00 +0800

    Pass the api url to the api constructor

--- src/gdaxClient.js
+++ src/gdaxClient.js
@@ -4,4 +4,4 @@ const { AuthenticatedClient } = require('gdax')
 const { sandbox } = require('./config')
 const apiUrl = sandbox ? 'https://api-public.sandbox.gdax.com' : 'https://api.gdax.com'
 
-module.exports = new AuthenticatedClient(process.env.GDAX_KEY, process.env.GDAX_SECRET, process.env.GDAX_PASSPHRASE)
+module.exports = new AuthenticatedClient(process.env.GDAX_KEY, process.env.GDAX_SECRET, process.env.GDAX_PASSPHRASE, apiUrl)
