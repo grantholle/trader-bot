ac6e4616f581415225f8453131e855a14f8d7134
Grant Holle <hollegrant@gmail.com>
2018-04-12 11:05:17 +0800

    Refactor the ticker handler to do buy trades more effectively, implement the new modules

--- src/handleTickerMessage.js
+++ src/handleTickerMessage.js
@@ -5,6 +5,8 @@ const { periods, granularities } = require('./config')
 const BigNumber = require('bignumber.js')
 const { last, clone } = require('lodash')
 const { percentChange, switchSide } = require('./utilities')
+const getAccounts = require('./accounts')
+const submitTrade = require('./trade')
 
 const largerPeriod = Math.max(...periods)
 const largerGranularity = Math.max(...granularities)
@@ -14,6 +16,8 @@ const smallerGranularity = Math.min(...granularities)
 let lastTickerPrice
 let numberOfTicksBelowEma = 0
 let numberOfTicksAboveEma = 0
+
+const MINIMUM_LOW_PRICE_TREND_UNTIL_TRADE = 2
 
 module.exports = (message, priceTracker) => {
   // If this isn't a ticker message or is and doesn't have a trade id
@@ -74,28 +78,33 @@ module.exports = (message, priceTracker) => {
     }
   }
 
+  // Holds the 2 emas of the smaller granularity
+  const [emaOne, emaTwo] = periods.map(p => new BigNumber(last(productData[smallerGranularity].indicators.ema[p])))
+  const smallerGranularityEmaPeriodsChange = percentChange(emaTwo, emaOne)
+  const currentPriceChangeFromSmallerEma = percentChange(emaOne, message.price)
+
   // Buying Logic:
   // Count the number of tick cycles where price is below and the smaller is lower
+  // else if the price has jumped relatively a lot, lower the count until it's back to zero
   if (priceIsBelowEma && smallerPeriodIsLower) {
     numberOfTicksBelowEma++
+  } else if (numberOfTicksBelowEma > 0 && currentPriceChangeFromSmallerEma.isGreaterThan(0.02)) {
+    numberOfTicksBelowEma--
   }
 
   // It has done this for at least 2? ticker cycles
-  if (numberOfTicksBelowEma > 1) {
-    const [emaOne, emaTwo] = periods.map(p => new BigNumber(last(productData[smallerGranularity].indicators.ema[p])))
+  if (numberOfTicksBelowEma >= MINIMUM_LOW_PRICE_TREND_UNTIL_TRADE) {
+    logger.debug(`${message.product_id}: Number of ticks where the trade price has been below all EMAs: ${numberOfTicksBelowEma}`)
+    logger.debug(`${message.product_id}: Smaller granularity EMA percent difference: ${smallerGranularityEmaPeriodsChange}`)
 
-    logger.debug(`${message.product_id}: numberOfTicksBelowEma=${numberOfTicksBelowEma}; emaOne=${emaOne}; emaTwo=${emaTwo}; percent change of emas=${percentChange(emaTwo, emaOne)}; percent change of ema and last price=${percentChange(emaOne, lastTickerPrice)}`)
+    // When both the smaller granularity EMAs are within -.01% of each other (meaning they are about to cross or have already)
+    // and have a very large negative or positive percent difference between the current trade price
+    if (smallerGranularityEmaPeriodsChange.isGreaterThanOrEqualTo(-0.01) && currentPriceChangeFromSmallerEma.isGreaterThanOrEqualTo(-0.001)) {
+      // Attempt to submit the trade
+      // We're wanting buying at one cent below the current trade price
+      submitTrade('buy', message.product_id, message.price.minus(0.01))
 
-    // When both the lower granularity EMAs are within .01% of each other (meaning they are about to cross)
-    // and have a zero or positive percent difference between the current trade price
-    if (percentChange(emaTwo, emaOne).abs().isGreaterThanOrEqualTo(0.01) &&
-      percentChange(emaOne, lastTickerPrice).isGreaterThanOrEqualTo(0)) {
-      // Check account balances if we can buy
-      // If we can, calculate the total coins we can buy based on the available USD
-      // put in a buy order for the current price less one cent
-      logger.info(`${message.product_id}: Place buy order for ${message.price.minus(0.01).toFixed(2)}`)
-
-      // reset numberOfTicksBelowEma
+      // reset down trending ticks back to zero
       numberOfTicksBelowEma = 0
     }
   }
