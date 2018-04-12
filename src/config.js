eee6d64d1ee1c2fa4222a159aa8af3230dd907ec
Grant Holle <hollegrant@gmail.com>
2018-04-12 10:57:03 +0800

    Add some defaults to the configuration

--- src/config.js
+++ src/config.js
@@ -2,10 +2,10 @@
 exports.products = process.env.GDAX_PRODUCTS.split(',')
 
 // The periods of which to calculate an EMA
-exports.periods = process.env.EMA_PERIODS.split(',').map(Number)
+exports.periods = process.env.EMA_PERIODS ? process.env.EMA_PERIODS.split(',').map(Number) : [12, 26]
 
 // The granularity of historical data (in seconds) https://docs.gdax.com/#get-historic-rates
-exports.granularities = process.env.GDAX_GRANULARITIES.split(',').map(Number)
+exports.granularities = process.env.GDAX_GRANULARITIES ? process.env.GDAX_GRANULARITIES.split(',').map(Number) : [60, 900]
 
 // Whether to use the sandbox api endpoints
 exports.sandbox = process.env.USE_SANDBOX === 'true'
