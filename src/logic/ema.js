5597b4e68d4b918e7c07e4b60d1bd94023592d4e
Grant Holle <hollegrant@gmail.com>
2018-04-22 22:34:29 +0800

    Ema logic fixes

--- src/logic/ema.js
+++ src/logic/ema.js
@@ -11,6 +11,8 @@ const smallerGranularity = Math.min(...granularities)
 
 module.exports = (message, priceTracker) => {
   const productData = priceTracker[message.product_id]
+  const priceBelowOtherEmas = true
+  const priceAboveOtherEmas = true
 
   for (const granularity of granularities) {
     // Check if the current price is above/below the ema's
@@ -18,6 +20,15 @@ module.exports = (message, priceTracker) => {
       const ind = productData[granularity].indicators[period]
       const lastEma = new BigNumber(last(ind.ema))
       const percent = percentChange(lastEma, message.price)
+
+      // Only count the slower 3/4 averages
+      if (granularity !== smallerGranularity && period !== smallerPeriod) {
+        if (percent.isPositive()) {
+          priceBelowOtherEmas = false
+        } else {
+          priceAboveOtherEmas = false
+        }
+      }
 
       logger.silly(`${message.product_id}: ${granularity / 60}min EMA${period} (${lastEma.toFixed(2)}) difference: ${percent.toFixed(2)}%`)
     }
@@ -31,9 +42,9 @@ module.exports = (message, priceTracker) => {
 
   // When both the smaller granularity EMAs are within -.01% of each other (meaning they are about to cross or have already)
   // and have a very large negative or positive percent difference between the current trade price
-  if (!positiveGain && priceAboveSmallEma && smallerGranularityEmaPeriodsChange.isGreaterThanOrEqualTo(-0.05)) {
+  if (!positiveGain && priceAboveSmallEma && priceBelowOtherEmas && smallerGranularityEmaPeriodsChange.isGreaterThanOrEqualTo(-0.05)) {
     return 'buy'
-  } else if (positiveGain && !priceAboveSmallEma && smallerGranularityEmaPeriodsChange.isLessThanOrEqualTo(0.05)) {
+  } else if (positiveGain && !priceAboveSmallEma && priceAboveOtherEmas && smallerGranularityEmaPeriodsChange.isLessThanOrEqualTo(0.05)) {
     // When both the smaller granularity EMAs are within .01% of each other (meaning they are about to cross or have already)
     // and have a very large negative or positive percent difference between the current trade price
     return 'sell'
