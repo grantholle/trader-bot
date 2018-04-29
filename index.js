a35a35190e2e025ec7631d6fb6d2d151c0ea1c2b
Grant Holle <hollegrant@gmail.com>
2018-04-29 19:21:33 +0800

    Async refactoring

--- index.js
+++ index.js
@@ -8,38 +8,35 @@ const init = require('./src/initialize')
 const tickerHandler = require('./src/ticker')
 const logger = require('./src/logger')
 const { products, granularities, liveTrade } = require('./src/config')
-const heartbeatReconnectDelay = 15000
 let reconnectAttempts = 0
-
-// Stores all candles, EMA data, and current-period candle data
-// This is the "data"
-let priceTracker = {}
-let heartbeatTimeout
 
 logger.info(`Trading is${liveTrade ? '' : ' NOT'} live`)
 
-gdaxWebsocket.on('open', () => {
+gdaxWebsocket.on('open', async () => {
   logger.info(`Connected to ${gdaxWebsocket.websocketURI}`)
+  let priceTracker = {}
+  let heartbeatTimeout
+  const heartbeatReconnectDelay = 15000
 
   heartbeatTimeout = setTimeout(gdaxWebsocket.connect, heartbeatReconnectDelay)
 
-  // Fetch the price history and calculate the
-  // current EMA for each granularity and period
-  init().then(tracker => {
-    priceTracker = tracker
-    reconnectAttempts = 0
+  try {
+    // Initialize by getting historical prices and starting intervals
+    priceTracker = await init()
+  } catch (err) {
+    logger.error(`Failed initializing`, err)
+  }
 
-    // Only attach the listener after we've been initialized
-    gdaxWebsocket.on('message', message => {
-      // Listen for hearbeats, reconnect if we lose the heartbeat after 15 seconds
-      if (message.type === 'heartbeat') {
-        clearTimeout(heartbeatTimeout)
-        heartbeatTimeout = setTimeout(gdaxWebsocket.connect, heartbeatReconnectDelay)
-      }
+  // Only attach the listener after we've been initialized
+  gdaxWebsocket.on('message', message => {
+    // Listen for hearbeats, reconnect if we lose the heartbeat after 15 seconds
+    if (message.type === 'heartbeat') {
+      clearTimeout(heartbeatTimeout)
+      heartbeatTimeout = setTimeout(gdaxWebsocket.connect, heartbeatReconnectDelay)
+    }
 
-      tickerHandler(message, priceTracker)
-    })
-  }).catch(logger.error)
+    tickerHandler(message, priceTracker)
+  })
 })
 
 gdaxWebsocket.on('close', () => {
@@ -59,7 +56,7 @@ gdaxWebsocket.on('error', err => {
   logger.error(`WebSocket error: ${err.message}`)
 
   // Attempt to reconnect after 30 seconds up to 4 times
-  if (reconnectAttempts < 5) {
+  if (++reconnectAttempts < 5) {
     setTimeout(gdaxWebsocket.connect, 30000)
   }
 })
--- src/initialize.js
+++ src/initialize.js
@@ -12,115 +12,88 @@ const smallerPeriod = Math.min(...periods)
 const smallerGranularity = Math.min(...granularities)
 const largerGranularity = Math.max(...granularities)
 
-module.exports = () => {
+module.exports = async () => {
   const priceTracker = {}
 
-  return new Promise(async (resolve, reject) => {
-    // Log the balances at the beginning of the trading period for kicks and giggles
-    accountBalances().catch(logger.error)
+  // Iterate the products
+  for (const product of products) {
+    priceTracker[product] = {}
 
-    // Iterate the products
-    for (const product of products) {
-      priceTracker[product] = {}
+    // Within the product, iterate the different granularities
+    for (const granularity of granularities) {
+      priceTracker[product][granularity] = {}
+      const tracker = priceTracker[product][granularity]
 
-      // Within the product, iterate the different granularities
-      for (const granularity of granularities) {
-        priceTracker[product][granularity] = {}
-        const tracker = priceTracker[product][granularity]
+      // Set some defaults for a trend
+      tracker.consecutiveUpCandles = 0
+      tracker.consecutiveDownCandles = 0
 
-        // Set some defaults for a trend
-        tracker.consecutiveUpCandles = 0
-        tracker.consecutiveDownCandles = 0
+      logger.verbose(`${product}: Getting historical data at every ${granularity / 60} minutes`)
 
-        logger.verbose(`${product}: Getting historical data at every ${granularity / 60} minutes`)
+      try {
+        // Make the request
+        const res = await client.getProductHistoricRates(product, { granularity })
 
-        try {
-          // Make the request
-          const res = await client.getProductHistoricRates(product, { granularity })
-
-          // Normalize all the results into something more retrievable
-          // We get the results newest -> oldest,
-          // so reverse that so it's oldest -> newest
-          tracker.allCandles = res.reverse().map(p => {
-            return {
-              low: p[1],
-              high: p[2],
-              open: p[3],
-              close: new BigNumber(p[4])
-            }
-          })
-
-          const totalResults = tracker.allCandles.length
-
-          // Trim the candles cache to the configured amount
-          if (process.env.PRICE_CACHE_SIZE < totalResults) {
-            tracker.allCandles.splice(0, totalResults - process.env.PRICE_CACHE_SIZE)
+        // Normalize all the results into something more retrievable
+        // We get the results newest -> oldest,
+        // so reverse that so it's oldest -> newest
+        tracker.allCandles = res.reverse().map(p => {
+          return {
+            low: p[1],
+            high: p[2],
+            open: p[3],
+            close: new BigNumber(p[4])
           }
+        })
 
-          logger.debug(`${product}: Total historical prices @ ${granularity / 60} minutes: ${totalResults}`)
+        const totalResults = tracker.allCandles.length
 
-          // Calculate the EMA using the historic prices
-          // for each of the configured periods
-          tracker.indicators = await getIndicators(product, granularity, tracker.allCandles.map(c => c.close.toNumber()))
-
-          const close = new BigNumber(tracker.allCandles[totalResults - 1].close)
-          tracker.currentCandle = {
-            open: close,
-            high: close,
-            low: close
-          }
-
-          // Start the interval at the granularity in ms
-          // To contribute to the running count and make calculations
-          tracker.interval = setInterval(async () => {
-            const c = tracker.currentCandle
-
-            logger.debug(`${product}: ${granularity / 60}min candle data: open=${c.open.toFixed(2)}, close=${c.close.toFixed(2)}, ${percentChange(c.open, c.close).toFixed(2)}% change, ${highLowSpread(c)} spread`)
-
-            // Add the last candle to the historical candles
-            tracker.allCandles.push(clone(c))
-
-            // Trim the allCandles down to the config size
-            if (process.env.PRICE_CACHE_SIZE < tracker.allCandles.length) {
-              tracker.allCandles.splice(0, tracker.allCandles.length - process.env.PRICE_CACHE_SIZE)
-            }
-
-            // Start the cycle again, with the previous close as the current open
-            c.open = c.close
-            c.low = c.close
-            c.high = c.close
-
-            // Recalculate the EMA
-            tracker.indicators = getIndicators(product, granularity, tracker.allCandles.map(c => c.close.toNumber()))
-
-            // Check if the candles are up from the previous one
-            tracker.lastCandleUp = c.close.isGreaterThanOrEqualTo(tracker.allCandles[tracker.allCandles.length - 2].close)
-            tracker.lastCandleDown = !tracker.lastCandleUp
-
-            // Only track this for the last 10 candles
-            // After 10 candles worth of data has been collected,
-            // The total of down and up candles should equal 10
-            if (tracker.lastCandleUp && tracker.consecutiveUpCandles < 10) {
-              tracker.consecutiveUpCandles++
-            } else if (tracker.consecutiveUpCandles > 0 && tracker.lastCandleDown) {
-              tracker.consecutiveUpCandles--
-            }
-
-            if (tracker.lastCandleDown && tracker.consecutiveDownCandles < 10) {
-              tracker.consecutiveDownCandles++
-            } else if (tracker.consecutiveDownCandles > 0 && tracker.lastCandleUp) {
-              tracker.consecutiveDownCandles--
-            }
-
-            logger.debug(`${product}: ${granularity / 60}min candle trended ${tracker.lastCandleDown ? 'down' : 'up'} from previous candle`)
-            logger.debug(`${product}: ${granularity / 60}min consecutive up candles: ${tracker.consecutiveUpCandles}; consecutive down ${tracker.consecutiveDownCandles}`)
-          }, granularity * 1000) // Granularity is in seconds
-        } catch (err) {
-          return reject(err)
+        // Trim the candles cache to the configured amount
+        if (process.env.PRICE_CACHE_SIZE < totalResults) {
+          tracker.allCandles.splice(0, totalResults - process.env.PRICE_CACHE_SIZE)
         }
+
+        logger.debug(`${product}: Total historical prices @ ${granularity / 60} minutes: ${totalResults}`)
+
+        // Calculate the EMA using the historic prices
+        // for each of the configured periods
+        tracker.indicators = await getIndicators(product, granularity, tracker.allCandles.map(c => c.close.toNumber()))
+
+        const close = new BigNumber(tracker.allCandles[totalResults - 1].close)
+        tracker.currentCandle = {
+          open: close,
+          high: close,
+          low: close
+        }
+
+        // Start the interval at the granularity in ms
+        // To contribute to the running count and make calculations
+        tracker.interval = setInterval(async () => {
+          const c = tracker.currentCandle
+
+          logger.debug(`${product}: ${granularity / 60}min candle data: open: $${c.open.toFixed(2)}, close: $${c.close.toFixed(2)}, low: $${c.low.toFixed(2)}, high: $${c.high.toFixed(2)}; ${percentChange(c.open, c.close).toFixed(2)}% change, ${highLowSpread(c)} spread`)
+
+          // Add the last candle to the historical candles
+          tracker.allCandles.push(clone(c))
+
+          // Trim the allCandles down to the config size
+          if (process.env.PRICE_CACHE_SIZE < tracker.allCandles.length) {
+            tracker.allCandles.splice(0, tracker.allCandles.length - process.env.PRICE_CACHE_SIZE)
+          }
+
+          // Start the cycle again, with the previous close as the current open
+          c.open = c.close
+          c.low = c.close
+          c.high = c.close
+
+          // Calculate the indicators
+          tracker.indicators = getIndicators(product, granularity, tracker.allCandles.map(c => c.close.toNumber()))
+        }, granularity * 1000) // Granularity is in seconds
+      } catch (err) {
+        return reject(err)
       }
     }
+  }
 
-    resolve(priceTracker)
-  })
+  return priceTracker
 }
