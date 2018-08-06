8d992cbc7cba7aa879f58ff00fa2a405d44039a4
Grant Holle <hollegrant@gmail.com>
2018-08-06 12:35:18 +0800

    Create the bot that will handle the trading and logic

--- src/bot.ts
+++ src/bot.ts
@@ -1,3 +1,63 @@
-export default class Bot {
+import coinbaseClient from './clients/coinbaseClient'
+import coinbaseWebsocket from './clients/coinbaseWebsocketClient'
+import Product from './product'
+import logger from './utilities/logger'
+import { clone } from 'lodash'
 
+export default class Bot {
+  public products: Array<Product>
+  private lastTickerPrice: BigNumber
+
+  async getProductData (products: Array<string>): Promise<void> {
+    const cbProducts = await coinbaseClient.getProducts()
+
+    for (const product of cbProducts) {
+      if (products.indexOf(product.id) !== -1) {
+        this.products.push(new Product(product))
+      }
+    }
+  }
+
+  handleTick (message: any): void {
+    // If this isn't a ticker message or is and doesn't have a trade id
+    if (message.type !== 'ticker' || (message.type === 'ticker' && !message.trade_id)) {
+      return
+    }
+
+    message.price = new BigNumber(message.price)
+
+    logger.verbose(`${message.product_id}: Trade: ${message.side} @ $${message.price.toFixed(2)}`)
+
+    // If it was the same price as last time, don't continue
+    if (this.lastTickerPrice && message.price.isEqualTo(this.lastTickerPrice)) {
+      return
+    }
+
+    // if (lastTickerPrice) {
+    //   priceLogger.info(`${message.product_id}: ${message.price.isGreaterThan(lastTickerPrice) ? '▲' : '▼'} ${message.price.toFixed(2)}`)
+    // }
+
+    const productData = priceTracker[message.product_id]
+
+    // Set the current candle price data for each granularity
+    for (const granularity of granularities) {
+      const candle = productData[granularity].currentCandle
+
+      candle.close = message.price
+
+      // Set the high and low for the candle
+      if (message.price.isLessThan(candle.low)) {
+        candle.low = message.price
+      }
+
+      if (message.price.isGreaterThan(candle.high)) {
+        candle.high = message.price
+      }
+    }
+
+    this.lastTickerPrice = clone(message.price)
+
+    // The module that handles the logic to make trades
+    traderLogic(message, productData)
+  }
 }
