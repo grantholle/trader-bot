28d1d8a020daa837912d6268ee95d458f69f27d7
Grant Holle <hollegrant@gmail.com>
2018-08-11 08:15:49 +0800

    Clean up granularity

--- src/granularity.ts
+++ src/granularity.ts
@@ -1,11 +1,14 @@
 import Candle from './candle'
+import Indicator from './indicator'
 
 export default class Granularity {
   public seconds: number
   public minutes: number
   public candles: Array<Candle>
-  public indicators: any
+  public currentCandle: Candle
+  public indicators: Array<Indicator>
   private candleCacheSize: number
+  private interval: NodeJS.Timer
 
   constructor (seconds: number) {
     this.seconds = seconds
@@ -17,5 +20,20 @@ export default class Granularity {
     if (this.candleCacheSize < this.candles.length) {
       this.candles.splice(0, this.candles.length - this.candleCacheSize)
     }
+  }
+
+  startInterval (): void {
+    this.interval = setInterval(() => {
+      this.candles.push(this.currentCandle)
+      this.trimCandles()
+
+      for (const indicator of this.indicators) {
+        indicator.analyze()
+      }
+    }, this.seconds * 1000)
+  }
+
+  clearInterval (): void {
+    clearInterval(this.interval)
   }
 }
