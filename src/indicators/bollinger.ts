13cbae66cb5378f1d8dd93d86c00687644e37001
Grant Holle <hollegrant@gmail.com>
2018-10-07 08:19:43 +0800

    Raise the standard deviation for bb calculation

--- src/indicators/bollinger.ts
+++ src/indicators/bollinger.ts
@@ -8,7 +8,7 @@ export default class BollingerBands implements Indicator {
 
   calculate (values: Array<number>) {
     const period: number = 12
-    const stdDev: number = 2
+    const stdDev: number = 2.3
 
     const bands = bollingerbands({
       period,
