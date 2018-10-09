2483b1f5b2f5bca9fe824ad9b98b02a8bcf66e46
Grant Holle <hollegrant@gmail.com>
2018-10-09 20:16:06 +0800

    Add the rsi strategy

---
+++ src/strategies/rsi.ts
@@ -0,0 +1,21 @@
+import Product from '../product'
+import { last } from 'lodash'
+import Strategy from './strategy'
+import CandleGranularity from '../granularity'
+import BigNumber from 'bignumber.js'
+
+export default class implements Strategy {
+  analyze (product: Product, indicatorData: any, granularity: CandleGranularity): any {
+    const lastRsi: BigNumber = last(indicatorData)
+
+    if (lastRsi.isGreaterThan(70)) {
+      return 'sell'
+    }
+
+    if (lastRsi.isLessThan(30)) {
+      return 'buy'
+    }
+
+    return null
+  }
+}
